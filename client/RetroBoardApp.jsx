33ab0d96daa39c60b0893b2f1902de029cd31977
Sean Everest <sean.b.everest@gmail.com>
2017-09-14 22:12:06 -0700

    filter out hidden cards

--- client/RetroBoardApp.jsx
+++ client/RetroBoardApp.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-import DeltaBoard from './DeltaBoard.jsx';
-import PlusBoard from './PlusBoard.jsx';
+import Delta from './Delta.jsx';
+import Plus from './Plus.jsx';
 
 class RetroBoardApp extends React.Component {
   static propTypes = {
@@ -15,8 +16,35 @@ class RetroBoardApp extends React.Component {
   render() {
     return (
       <div className="retro-container">
-        <PlusBoard pluses={this.props.pluses} />
-        <DeltaBoard deltas={this.props.deltas} />
+        <TransitionGroup className="retro-container--plus">
+          {this.props.pluses.filter(p => !p.hide).map((p, i) =>
+            <CSSTransition
+              key={p.id}
+              timeout={200}
+              classNames="fade"
+            >
+              <Plus
+                index={i}
+                {...p}
+              />
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+
+        <TransitionGroup className="retro-container--delta">
+          {this.props.deltas.filter(d => !d.hide).map((d, i) =>
+            <CSSTransition
+              key={d.id}
+              timeout={200}
+              classNames="fade"
+            >
+              <Delta
+                index={i}
+                {...d}
+              />
+            </CSSTransition>
+          )}
+        </TransitionGroup>
       </div>
     );
   }
