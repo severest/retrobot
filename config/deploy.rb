ca0dbe6a1334831a051806a308f8885ff1d718f5
Sean Everest <sean.b.everest@gmail.com>
2018-02-05 21:46:35 -0800

    deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,8 +29,5 @@ append :linked_files, 'config/database.yml', 'config/secrets.yml'
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'node_modules'
 
-# Default value for default_env is {}
-set :default_env, { path: "/usr/local/src/nvm/versions/node/v6.9.1/bin:/usr/local/bin:$PATH" }
-
 # Default value for keep_releases is 5
 set :keep_releases, 3
