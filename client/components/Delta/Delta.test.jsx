c4fb6c9416f0d7bff78bab7b5321e350ada7d76f
Sean Everest <sean.b.everest@gmail.com>
2018-02-13 23:00:40 -0800

    some enzyme testing on pluses and deltas

--- client/components/Delta/Delta.test.jsx
+++ client/components/Delta/Delta.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import Delta from './Delta.jsx';
+import { RETRO_STATUS } from '../../utils/constants.js';
 
 const PROPS = {
   id: 1,
@@ -9,10 +11,38 @@ const PROPS = {
   index: 1,
   votes: 0,
   retroKey: '123456',
+  retroState: RETRO_STATUS.IN_PROGRESS,
   showOpenNotesBtn: true,
 };
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Delta {...PROPS} />, div);
+});
+
+it('shows controls when in progress', () => {
+  const wrapper = shallow(<Delta {...PROPS} retroState={RETRO_STATUS.IN_PROGRESS} />);
+  expect(wrapper.find('.upvote').length).toBe(1);
+  expect(wrapper.find('.downvote').length).toBe(1);
+  expect(wrapper.find('.notes').length).toBe(0);
+  expect(wrapper.find('.card__delete').length).toBe(1);
+  expect(wrapper.find('.card__votes').length).toBe(0);
+});
+
+it('shows controls when voting', () => {
+  const wrapper = shallow(<Delta {...PROPS} retroState={RETRO_STATUS.VOTING} />);
+  expect(wrapper.find('.upvote').length).toBe(1);
+  expect(wrapper.find('.downvote').length).toBe(1);
+  expect(wrapper.find('.notes').length).toBe(0);
+  expect(wrapper.find('.card__delete').length).toBe(1);
+  expect(wrapper.find('.card__votes').length).toBe(0);
+});
+
+it('shows controls when locked', () => {
+  const wrapper = shallow(<Delta {...PROPS} retroState={RETRO_STATUS.LOCKED} />);
+  expect(wrapper.find('.upvote').length).toBe(0);
+  expect(wrapper.find('.downvote').length).toBe(0);
+  expect(wrapper.find('.card__votes').length).toBe(1);
+  expect(wrapper.find('.notes').length).toBe(1);
+  expect(wrapper.find('.card__delete').length).toBe(0);
 });
--- client/components/Plus/Plus.test.jsx
+++ client/components/Plus/Plus.test.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import Plus from './Plus.jsx';
+import { RETRO_STATUS } from '../../utils/constants.js';
 
 const PROPS = {
   id: 1,
   content: 'plus content',
   index: 1,
   retroKey: '123456',
+  retroState: RETRO_STATUS.IN_PROGRESS,
 };
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Plus {...PROPS} />, div);
+});
+
+it('shows controls when in progress', () => {
+  const wrapper = shallow(<Plus {...PROPS} retroState={RETRO_STATUS.IN_PROGRESS} />);
+  expect(wrapper.find('.card__delete').length).toBe(1);
+});
+
+it('shows controls when voting', () => {
+  const wrapper = shallow(<Plus {...PROPS} retroState={RETRO_STATUS.VOTING} />);
+  expect(wrapper.find('.card__delete').length).toBe(1);
+});
+
+it('shows controls when locked', () => {
+  const wrapper = shallow(<Plus {...PROPS} retroState={RETRO_STATUS.LOCKED} />);
+  expect(wrapper.find('.card__delete').length).toBe(0);
 });
