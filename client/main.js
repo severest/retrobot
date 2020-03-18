68121f9b555daa4baa74c1b24649f7c9e6488b83
Sean Everest <me@seaneverest.com>
2020-03-18 09:51:05 -0700

    viewport height on mobile issue

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import debounce from 'lodash/debounce';
 
 import './styles/application.scss';
 
@@ -10,13 +11,13 @@ import {
   isOnline,
   isOffline,
 } from './flux/retro/actions.js';
+import { setViewportUnits } from './utils/viewport.js';
 
 import StartRetroApp from './StartRetroApp-Loadable.jsx';
 import RetroBoardApp from './RetroBoardApp-Loadable.jsx';
 import TeamSummaryApp from './TeamSummaryApp-Loadable.jsx';
 import Particles from './components/Particles/Particles.jsx';
-
-
+// User ID generator
 let userId = localStorage.getItem('retrobotID');
 if (!userId) {
   userId = uuid();
@@ -24,14 +25,21 @@ if (!userId) {
 }
 window.myID = userId;
 
+// Online/Offline indicators
 window.addEventListener('online', () => isOnline());
 window.addEventListener('offline', () => isOffline());
 
+// Viewport variables
+const viewportUpdateDebounced = debounce(setViewportUnits, 100);
+window.addEventListener('resize', viewportUpdateDebounced);
+setViewportUnits();
 
+// 404
 const NoMatch = () => (
   <Particles code="404" />
 );
 
+///// Setup router
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
---
+++ client/utils/viewport.js
@@ -0,0 +1,7 @@
+export const setViewportUnits = () => {
+  // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
